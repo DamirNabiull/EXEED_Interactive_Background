9fa36017e2476e632610a64e54a8584ef52c1a9c
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-10-08 03:17:14 +0300

    Services are located on server

---
+++ Application/upload_server.py
@@ -0,0 +1,74 @@
+from cryptography.fernet import Fernet
+from aiohttp import web
+import json
+import yadisk
+import re
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib as smtp
+
+y_disk: yadisk.YaDisk
+email: str
+smtp_pass: str
+token: str
+
+routes = web.RouteTableDef()
+@routes.post("/uploadAndSend")
+async def post_upload(request):
+    global y_disk, email, smtp_pass
+
+    data = await request.json()
+    final_video, to_send_email, current_id = data['final_video'], data['to_send_email'], data['current_id']
+
+    print('************* UPLOAD VIDEO *************')
+    dir = config['disk_dir']
+    dest_path = f'/{dir}/{current_id}.mp4'
+    print('UPLOAD')
+    try:
+        y_disk.upload(final_video, dest_path, timeout=10)
+    except Exception as e:
+        print(e)
+    print('UPLOADED')
+    video_url = y_disk.get_download_link(dest_path)
+    print('GET LINK')
+    print(video_url)
+
+    print('Send started')
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", to_send_email):
+        pass
+    else:
+        server = smtp.SMTP_SSL('smtp.yandex.com', 465)
+        server.set_debuglevel(1)
+        server.ehlo(email)
+        server.login(email, smtp_pass)
+        server.auth_plain()
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Exeed Video"
+        msg['From'] = email
+        msg['To'] = to_send_email
+        part1 = MIMEText(video_url, 'plain')
+        msg.attach(part1)
+
+        server.sendmail(email, to_send_email, msg.as_string())
+        server.quit()
+    print('Send ended')
+
+    data = {'status': 200}
+    return web.json_response(data=data)
+
+
+if __name__ == '__main__':
+    config = json.load(open('config.json'))
+    key = b'-HzoIZjl0UaJfzRdTW-LD-ik9q96yvEioeH9RME1XHs='
+    cipher_suite = Fernet(key)
+    smtp_pass = cipher_suite.decrypt(config['smtp_pass'].encode()).decode()
+    token = cipher_suite.decrypt(config['token'].encode()).decode()
+    email = config['login']
+
+    y_disk = yadisk.YaDisk(token=token)
+    print(y_disk.check_token())
+
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, host='0.0.0.0', port=8080)
